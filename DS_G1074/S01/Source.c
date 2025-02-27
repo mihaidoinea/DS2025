e864f1111dda09aba7c45748b2505e5a1e2a438e
Mihai Doinea <mihai.doinea@oracle.com>
2025-02-27 11:12:50 +0200

    G1067_S01

--- DS_G1074/S01/Source.c
+++ DS_G1074/S01/Source.c
@@ -1,4 +1,34 @@
+#include "stdio.h"
+void switchNumbers(int* a, int* b)
+{
+	printf("Switch stack before:\n");
+	printf("&a = 0x%p, &b = 0x%p\n", &a, &b);
+	printf("a = 0x%p, b = 0x%p\n\n", a, b);
+	printf("*a = %d, *b = %d\n\n", *a, *b);
+
+	int c = *a;
+	*a = *b;
+	*b = c;
+
+	printf("Switch stack after:\n");
+	printf("&a = 0x%p, &b = 0x%p\n", &a, &b);
+	printf("a = 0x%p, b = 0x%p\n\n", a, b);
+	printf("*a = %d, *b = %d\n\n", *a, *b);
+
+
+}
 void main()
 {
+	int x = 7;
+	int y = 8;
 
+	printf("Main stack before:\n");
+	printf("&x = 0x%p, &y = 0x%p\n", &x, &y);
+	printf("x = %d, y = %d\n\n", x, y);
+
+	switchNumbers(&x, &y);
+
+	printf("Main stack after:\n");
+	printf("&x = 0x%p, &y = 0x%p\n", &x, &y);
+	printf("x = %d, y = %d\n", x, y);
 }
