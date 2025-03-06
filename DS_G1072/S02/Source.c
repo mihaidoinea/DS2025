edcf17295229ae68fd07ea7e95a7b16fefe81b60
Mihai Doinea <mihai.doinea@oracle.com>
2025-03-06 14:43:43 +0200

    G1072_S02

--- DS_G1072/S02/Source.c
+++ DS_G1072/S02/Source.c
@@ -2,11 +2,10 @@
 void main()
 {
 	Student* stud = NULL;
-	stud = createStudent("Popescu Maria", 1072, 1900.3);
+	stud = createStudent("Popescu Maria", 1072, 1900.3f);
 	printf("Student: %s, group: %d, income: %.2f\n",
 		stud->name, stud->group, stud->income);
 	
-
 	Student* arrayStud[5];
 	arrayStud[0] = stud;
 
