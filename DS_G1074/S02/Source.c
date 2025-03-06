e4051a8a63f6d70c8ab3b30cd5b3bb3ea84b9b48
Mihai Doinea <mihai.doinea@oracle.com>
2025-03-06 10:10:41 +0200

    G1074_S02

---
+++ DS_G1074/S02/Source.c
@@ -0,0 +1,9 @@
+#include "Student.h"
+void main()
+{
+	Student* stud = NULL;
+	stud = createStudent("Popescu Maria", 1074, 1200.4);
+	printf("Student: %s, group: %d, income: %.2f\n",
+		stud->name, stud->group, stud->income);
+
+}
