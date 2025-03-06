a02c2cbe9314e0f86bd9c237ef63fd87b43805fd
Mihai Doinea <mihai.doinea@oracle.com>
2025-03-06 14:36:21 +0200

    G1074_S02

--- DS_G1074/S02/Source.c
+++ DS_G1074/S02/Source.c
@@ -2,7 +2,8 @@
 void main()
 {
 	Student* stud = NULL;
-	stud = createStudent("Popescu Maria", 1074, 1200.4);
+	stud = createStudent("Popescu Maria", 1074, 1200.4f);
 	printf("Student: %s, group: %d, income: %.2f\n",
 		stud->name, stud->group, stud->income);
+	deleteStudent(stud);
 }
