f721f134f03287cfc2dacd6d8c536f4449ae95e3
Mihai Doinea <mihai.doinea@oracle.com>
2025-03-12 16:24:04 +0200

    G1073_S03

--- DS_G1073/S03/Source.c
+++ DS_G1073/S03/Source.c
@@ -1,8 +1,37 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include "Student.h"
 #define LINE_SIZE 256
+
+void insertStud(Student*** students, Student* stud, int* noStudents)
+{
+	Student** buffer = NULL;
+	(*noStudents)++;
+	buffer = (Student**)malloc((*noStudents) * sizeof(Student*));
+	if (buffer != NULL)
+	{
+		memset(buffer, 0, (*noStudents) * sizeof(Student*));
+
+		for (register int i = 0; i < (*noStudents) - 1; i++)
+		{
+			buffer[i] = (*students)[i];
+		}
+		buffer[(*noStudents) - 1] = stud;
+		if (*students != NULL)
+		{
+			free(*students);
+		}
+		*students = buffer;
+	}
+}
+
 void main()
 {
+	Student ** students = NULL;
+	//pointer to pointer to Student - 1 el.
+	//array of pointers to Student
+	//matrix of elements of type Student
+	int noStudents = 0;
+
 	FILE* pFile = fopen("Data.txt", "r");
 	if (pFile != NULL)
 	{
@@ -25,9 +54,16 @@ void main()
 
 			Student* stud = createStudent(name, group, income);
 			printStudent(stud);
-			
-			deleteStudent(stud);
-		}
 
+			//insert element into the array
+			insertStud(&students, stud, &noStudents);
+
+			//deleteStudent(stud);
+		}
+		for (int i = 0; i < noStudents; i++)
+		{
+			printStudent(students[i]);
+			deleteStudent(students[i]);
+		}
 	}
 }
