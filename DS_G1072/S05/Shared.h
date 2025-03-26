4dcf478dc88ece65ed2190710689d17ae30eb0eb
Mihai Doinea <mihai.doinea@oracle.com>
2025-03-26 08:57:33 +0200

    G1072_S05

---
+++ DS_G1072/S05/Shared.h
@@ -0,0 +1,28 @@
+#pragma once
+#include "stdio.h"
+#include "stdlib.h"
+#include "string.h"
+
+struct _Student
+{
+	short group;
+	char* name;
+	float income;
+};
+typedef struct _Student		Student;
+typedef struct _Student*	PStudent;
+
+struct _Node
+{
+	Student* info;
+	struct _Node* next;
+};
+typedef struct _Node	Node;
+typedef struct _Node*	PNode;
+
+Student* createStudent(const char*, short, float);
+void deleteStudent(PStudent);
+void printStudent(PStudent);
+void insertHeadList(Node**, Student*);
+//void insertHeadList(PNode*, pStud);
+void printList(PNode);
