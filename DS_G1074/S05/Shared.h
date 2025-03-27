aef6abfb280d738dae048316fc8d1617ac5e1b8d
Mihai Doinea <mihai.doinea@oracle.com>
2025-03-27 08:58:02 +0200

    G1074_S05

---
+++ DS_G1074/S05/Shared.h
@@ -0,0 +1,32 @@
+#pragma once
+#include "stdio.h"
+#include "stdlib.h"
+#include "string.h"
+struct _Student
+{
+	char* name;
+	float income;
+	short group;
+};
+typedef struct _Student		Student;
+typedef struct _Student*	PStudent;
+
+//definition of a SLL
+struct _Node
+{
+	Student* info;
+	struct _Node* next;
+};
+
+typedef struct _Node	Node;
+typedef struct _Node*	PNode;
+
+Student* createStudent(const char*, short, float);
+void deleteStudent(PStudent);
+void printStudent(PStudent);
+
+//simple linked lists functions
+Node* insertHeadList(Node*, Student*);
+void insertTailList(Node**, Student*);
+void printSimpleList(Node*);
+void deleteSimpleList(Node**);
