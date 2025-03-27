b819f0b8408ea3d2135d5311a2bcbf62259ba713
Mihai Doinea <mihai.doinea@oracle.com>
2025-03-27 12:04:04 +0200

    G1066_S05

---
+++ DS_G1066/S05/Shared.h
@@ -0,0 +1,31 @@
+#pragma once
+#include "stdio.h"
+#include "stdlib.h"
+#include "string.h"
+
+struct _Student
+{
+	float income;
+	short group;
+	char* name;
+};
+typedef struct _Student Student;
+typedef struct _Student* PStudent;
+
+struct _Node
+{
+	Student* info;
+	struct _Node* next;
+};
+
+typedef struct _Node	Node;
+typedef struct _Node*	PNode;
+
+Student* createStudent(const char*, short, float);
+void deleteStudent(PStudent);
+void printStudent(PStudent);
+
+//simple lists functions
+void insertTailList(Node**, Student*);
+void printSimpleList(Node*);
+void deleteStudByName(Node**, const char*);
---
+++ DS_G1066/S05/Source.c
@@ -0,0 +1,33 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include "Shared.h"
+#define LINE_SIZE 256
+
+void main()
+{
+	Node* pHeadList = NULL;
+
+	FILE* pFile = fopen("Data.txt", "r");
+	if (pFile != NULL)
+	{
+		char buffer[LINE_SIZE], name[LINE_SIZE];
+		char* token; float income; int group;
+
+		char delimiter[] = { ',','\0' };
+		while (fgets(buffer, LINE_SIZE, pFile))
+		{
+			token = strtok(buffer, delimiter);
+			strcpy(name, token);
+
+			token = strtok(NULL, delimiter);
+			group = atoi(token);
+
+			token = strtok(NULL, delimiter);
+			income = (float)atof(token);
+
+			Student* stud = createStudent(name, group, income);
+
+
+		}
+	}
+}
+
