41cce9277dd63523624f918a1bbb34331fc9ee76
Mihai Doinea <mihai.doinea@oracle.com>
2025-04-02 14:35:15 +0300

    G1072_S06

--- DS_G1072/S06/Shared.h
+++ DS_G1072/S06/Shared.h
@@ -34,3 +34,20 @@ void pushStudentHeadList(Node**, Student*);
 Student* popStudentTailList(Node**);
 Student* popStudentHeadList(Node**);
 void deleteStack(Node**);
+
+//sparse matrix (square) of pointers to Student
+//node article for a sparse list
+typedef struct _SparseNode
+{
+	int row;
+	int column;
+	Student* info;
+	struct _SparseNode* next;
+} SparseNode;
+
+
+//I: a passed by value array of pointers to Student (Student**)
+//I: the number of elements
+//O: the function returns a matrix of pointers to Student
+Student*** loadMatrix(Student**, int);
+void convertToSparseList(Student***, int, SparseNode*);
--- DS_G1072/S06/Source.c
+++ DS_G1072/S06/Source.c
@@ -1,8 +1,12 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include "Shared.h"
+#include "math.h"
 #define LINE_SIZE 256
+#define NO_STUDENTS 9
 void main()
 {
+	Student* students[NO_STUDENTS];
+
 	FILE* pFile = fopen("Data.txt", "r");
 	if (pFile != NULL)
 	{
@@ -12,6 +16,7 @@ void main()
 		float income;
 		short group;
 		char* token = NULL;
+		int index = 0;
 		while (fgets(buffer, LINE_SIZE, pFile))
 		{
 			token = strtok(buffer, delimiter);
@@ -27,7 +32,33 @@ void main()
 			//printf("Income: %.2f\n", income);
 
 			Student* pStud = createStudent(name, group, income);
-
+			students[index++] = pStud;
 		}
+		int noRowsCols = (int)sqrt(NO_STUDENTS);
+		Student*** sparseMatrix = loadMatrix(students, noRowsCols);
+
+		SparseNode* listHead = NULL;
+		convertToSparseList(sparseMatrix, noRowsCols, listHead);
+
 	}
+}
+
+void convertToSparseList(Student*** sparseMatrix,
+	int noRowsCols, SparseNode* listHead)
+{
+
+}
+
+Student*** loadMatrix(Student** students, int noRowsCols)
+{
+	Student*** matrix = NULL;
+	matrix = (Student***)malloc(sizeof(Student**)* noRowsCols);
+	for (int i = 0; i < noRowsCols; i++)
+	{
+		matrix[i] = (Student**)malloc(sizeof(Student*) * noRowsCols);
+		memset(matrix[i], 0, sizeof(Student*) * noRowsCols);
+		for (int j = 0; j < noRowsCols; j++)
+			matrix[i][j] = students[i * noRowsCols + j];
+	}
+	return matrix;
 }
