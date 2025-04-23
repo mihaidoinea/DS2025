1189d335e00d5f6b48b55327da6a9d987a6d86da
Mihai Doinea <mihai.doinea@oracle.com>
2025-04-23 11:27:09 +0300

    G1073_S08

---
+++ DS_G1073/S08/Student.c
@@ -0,0 +1,42 @@
+#include "Shared.h"
+
+Student* createStudent(const char* name, short group, float income)
+{
+	//var definition
+	PStudent stud = NULL;
+	//allocate memory
+	stud = (Student*)malloc(sizeof(Student));
+	if (stud != NULL)
+	{
+		//init struct attributes
+		stud->group = group;
+		stud->income = income;
+		stud->name = (char*)malloc(strlen(name) + 1);
+		if (stud->name != NULL)
+		{
+			strcpy_s(stud->name, strlen(name) + 1, name);
+		}
+	}
+	//return value
+	return stud;
+}
+void deleteStudent(PStudent pStud)
+{
+	if (pStud != NULL)
+	{
+		if (pStud->name != NULL)
+			free(pStud->name);
+		free(pStud);
+	}
+}
+void printStudent(PStudent pStud)
+{
+	if (pStud != NULL)
+	{
+		if (pStud->name != NULL)
+			printf("Name: %s\n", pStud->name);
+		printf("Group: %d\n", pStud->group);
+		printf("Income: %.2f\n", pStud->income);
+	}
+	printf("\n");
+}
