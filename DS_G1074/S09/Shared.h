536c8dc12408e5a9a7a33d4e8dc10d81d195d953
Mihai Doinea <mihai.doinea@oracle.com>
2025-05-08 09:06:49 +0300

    G1074_S09

---
+++ DS_G1074/S09/Shared.h
@@ -0,0 +1,55 @@
+#pragma once
+#include "stdio.h"
+#include "stdlib.h"
+#include "string.h"
+struct _Student
+{
+	char* name;
+	float income;
+	short group;
+};
+typedef struct _Student		Student;
+typedef struct _Student*	PStudent;
+
+//definition of a SLL
+struct _Node
+{
+	Student* info;
+	struct _Node* next;
+	struct _Node* prev;
+};
+
+typedef struct _Node	Node;
+typedef struct _Node*	PNode;
+
+Student* createStudent(const char*, short, float);
+void deleteStudent(PStudent);
+void printStudent(PStudent);
+
+//simple linked lists functions
+Node* insertHeadList(Node*, Student*);
+void insertTailList(Node**, Student*);
+void printSimpleList(Node*);
+void deleteSimpleList(Node**);
+
+//STACK functions
+void pushStudent(Node**, Student*);
+Student* popStudent(Node**);
+Student* peekStudent(Node*);
+
+//doubly linked list
+void convertToSortedDoublyLinkedList(Student**, Node**);
+
+//HashTable definitions
+
+#define HASH_SIZE 26
+typedef struct HashTable
+{
+	Node** buckets;
+	int size;
+}HashTable;
+
+void putStudent(HashTable*, Student*);
+Student* getStudent(HashTable, const char*);
+void deleteStudentByKey(HashTable, const char*);
+void printHashTable(HashTable);
---
+++ DS_G1074/S09/Source.c
@@ -0,0 +1,32 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include "Shared.h"
+#define LINE_SIZE 256
+#define MAX_STUDENTS 10
+
+void main()
+{
+
+	FILE* pFile = fopen("Data.txt", "r");
+	if (pFile != NULL)
+	{
+		char delimiter[] = { ',','\0' };
+		char name[LINE_SIZE], buffer[LINE_SIZE];
+		float income; int group;
+		char* token;
+		int noStud = 0, index =0;
+		while (fgets(buffer, LINE_SIZE, pFile))
+		{
+			token = strtok(buffer, delimiter);
+			strcpy(name, token);
+
+			token = strtok(NULL, delimiter);
+			group = atoi(token);
+
+			token = strtok(NULL, delimiter);
+			income = (float)atof(token);
+
+			Student* stud = createStudent(name, group, income);
+
+		}
+	}
+}
